4db7f1cb4f184d1b130af9725442eb8d3066fdff
kkirerot <142846253+kkirerot@users.noreply.github.com>
2023-08-23 00:06:28 +0200

    remove unique customer ssn in DB

--- applications/loan_application/models.py
+++ applications/loan_application/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class LoanApplication(models.Model):
-    customer_ssn = models.CharField(max_length=11, unique=True)
+    customer_ssn = models.CharField(max_length=11)
     full_name = models.CharField(max_length=255)
     loan_amount = models.IntegerField()
     equity_amount = models.IntegerField()
